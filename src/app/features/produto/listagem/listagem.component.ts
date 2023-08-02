5c8c02d2c2cb08c6f94364dd00516cd38ae6eb58
keidsondesigner <keidsondesigner@gmail.com>
2023-08-02 19:17:58 -0400

    navegação por id

--- src/app/features/produto/listagem/listagem.component.ts
+++ src/app/features/produto/listagem/listagem.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { Observable, tap } from 'rxjs';
 import { CursoService } from 'src/app/services/curso.service';
 
 @Component({
@@ -8,15 +10,25 @@ import { CursoService } from 'src/app/services/curso.service';
   styleUrls: ['./listagem.component.scss']
 })
 export class ListagemComponent implements OnInit {
+  displayedColumns: string[] = ['categoria', 'curso', 'acoes'];
   courses$!: Observable<any>;
+  // cursos!: any[];
 
-  constructor( private _coursesService: CursoService ){};
+  constructor( private _coursesService: CursoService, private router: Router ){};
 
   ngOnInit(): void {
-    this.courses$ = this._coursesService.getAllCourses();
-    // this._coursesService.getAllCourses().subscribe( dados => {
-    //   console.log('retorno funçao', dados);
+    this.courses$ = this._coursesService.getAllCourses().pipe(tap(
+      retorno => console.log('chamada do método', retorno)
+    ));
+    // this._coursesService.getAllCourses().subscribe( cursos => {
+    //   console.log('retorno funçao', cursos);
+    //   this.cursos = cursos;
     // });
+  }
+
+  editar(course: any){
+    this.router.navigate(['produto/editar-produto', course.id]);
+    console.log('editar');
   }
 
 }
--- src/app/features/produto/produto-routing.module.ts
+++ src/app/features/produto/produto-routing.module.ts
@@ -6,7 +6,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 const routes: Routes = [
   { path: '', redirectTo: 'listagem', pathMatch: 'full'},
   { path: 'listagem', component: ListagemComponent},
-  { path: 'novo-produto', component: CadastroComponent}
+  { path: 'novo-produto', component: CadastroComponent},
+  { path: 'editar-produto/:id', component: CadastroComponent},
 ];
 
 @NgModule({
