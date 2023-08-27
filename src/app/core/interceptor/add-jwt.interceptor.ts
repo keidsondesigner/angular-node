02306855336a39eb20cd78371a89dc6de9477c8e
keidsondesigner <keidsondesigner@gmail.com>
2023-08-27 17:15:40 -0400

    feat:('tratamento de erro jwt: #498 ao efetuar login sem token; #401 não autorizado, não possui token;')

--- src/app/core/interceptor/add-jwt.interceptor.ts
+++ src/app/core/interceptor/add-jwt.interceptor.ts
@@ -3,23 +3,39 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class AddJwtInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private router: Router, private toastrNotifier: ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = localStorage.getItem('token');
-    console.log(token);
+    console.log('aqui token', token);
 
     if(token) {
       request = request.clone({ setHeaders: { Authorization: 'Bearer ' + token }})
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if(error.status === 401) {
+          console.log(error);
+          console.log(error.error);
+          this.toastrNotifier.error('token não fornecido', 'Error ao efetuar login!');
+          this.router.navigate(['/pages/auth/login']);
+        } else if(error.status ===  498) {
+          this.router.navigate(['/pages/auth/login']);
+          this.toastrNotifier.error('Efetue o login corretamente', 'Token inválido!');
+        }
+        return throwError(() => new Error('Error'));
+      })
+    );
   }
 }
