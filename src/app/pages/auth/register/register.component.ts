df568cb83c594a4c8482fbf2c8bf846fda852d8e
keidsondesigner <keidsondesigner@gmail.com>
2023-09-02 11:51:13 -0400

    style: 'Atulizando component REGISTER com TailwindCSS'

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,17 +1,18 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { first } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { NavbarService } from 'src/app/core/services/navbar.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   form!: FormGroup;
   hide = true;
 
@@ -22,18 +23,19 @@ export class RegisterComponent implements OnInit {
       private formBuilder: FormBuilder,
       private router: Router,
       private authService: AuthService,
-      private toastrNotifier: ToastrService
-      // private alertService: AlertService
+      private toastrNotifier: ToastrService,
+      private navbarService: NavbarService
   ) {
     this.initForm();
   }
 
   ngOnInit() {
+    this.navbarService.hide();
   }
 
   initForm() {
     this.form = this.formBuilder.group({
-      name: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(4)]],
       email: ['', Validators.email],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
@@ -70,5 +72,9 @@ export class RegisterComponent implements OnInit {
 
     //reset form submit
     // this.form.reset();
+  }
+
+  ngOnDestroy() {
+    this.navbarService.show();
   }
 }
