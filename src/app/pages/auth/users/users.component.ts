951f0b7b66a42d1e6ee60968e6e0d53741824fc1
keidsondesigner <keidsondesigner@gmail.com>
2023-09-03 15:52:10 -0400

    chore: 'loading temporario'

--- src/app/pages/auth/users/users.component.ts
+++ src/app/pages/auth/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, first, map, tap } from 'rxjs';
 import { IUser } from 'src/app/core/models/user.model';
 import { AuthService } from 'src/app/core/services/auth.service';
 
@@ -9,17 +9,30 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  users$!: Observable<IUser[]>;
+  // users$!: Observable<IUser[]>;
 
-  displayedColumns: string[] = ['id', 'name', 'email'];
+  //código temporario até implementação do NgRx
+  users$!: IUser[];
+
+  loading = true;
+
 
   constructor(private _authService: AuthService,) { }
 
   ngOnInit(): void {
-    this.users$ = this._authService.getAllUsers().pipe(
-        tap(data => console.log('retorno data antes do map(): ', data)),
-        map(data => data.users),
-        tap(data => console.log('retorno data depois do map(): ', data))
-      );
+    // this.users$ = this._authService.getAllUsers().pipe(
+    //     tap(data => console.log('retorno data antes do map(): ', data)),
+    //     map(data =>  data.users),
+    //     tap(data => console.log('retorno data depois do map(): ', data)),
+    //   );
+
+    //código temporario até implementação do NgRx
+    this._authService.getAllUsers().pipe(first()).subscribe({
+      next: (result) => {
+        this.loading = false
+        console.log('result', result.users);
+        this.users$ = result.users;
+      }
+    });
   }
 }
